cedeb5ddcc42fe7a726b3391e766d79724122c28
Andra Cardas <andra.cardas@altom.com>
2022-02-15 12:09:08 +0200

    ios build and script

--- run-tests_android.sh
+++ run-tests_android.sh
@@ -9,7 +9,7 @@ appium --log-no-colors --log-timestamp  --command-timeout 60  > appium.log 2>&1 
 sleep 10
 ps -ef|grep appium
 
-export APPIUM_APPFILE=$PWD/TrashCat.apk #App file is at current working folder
+export APPIUM_APPFILE=$PWD/android/TrashCat.apk #App file is at current working folder
 
 ## Desired capabilities:
 export APPIUM_URL="http://localhost:4723/wd/hub"
